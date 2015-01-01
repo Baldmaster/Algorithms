295bc91638df19abcc26e9ec2c17510c09a03ef6
Baldmaster <sergey.archlinux@gmail.com>
2015-01-01 20:59:11 +0200

    insertonsort.h created

---
+++ insertionsort.h
@@ -0,0 +1,23 @@
+/* insertion sort */
+#ifndef INSERTSORT_H
+#define INSERTSORT_H
+
+/* includes */
+#include <stdio.h>
+#define LENGTH(a) (sizeof(a) / sizeof(a[0]))
+
+void insertion_sort (int arr[], int len) {
+    int i, j, key;
+  
+    for (i = 1; i < len; i++) {
+        key = arr[i];
+        j = i - 1;
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+	}
+        arr[j + 1] = key;
+    }
+}
+
+#endif

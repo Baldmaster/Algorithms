d41e8d78df16c9de615f1ec87d847f99b4ac6a15
Baldmaster <sergey.archlinux@gmail.com>
2015-01-03 15:17:00 +0200

    linkedlist refactoring

---
+++ linkedlist.c
@@ -0,0 +1,100 @@
+/* Example of using linked list *
+ * for any data structure       */
+
+#include "linkedlist.h"
+
+typedef struct _complex {
+    int real;
+    int im;
+} complex;
+
+
+void show_complex (complex*);
+void show_integer (int*);
+int compare_complex (complex*, complex*);
+int compare_integers (int*, int*);
+
+int main (void) {
+    /* create new list of complex numbers and initialize it */
+    linked_list c_list;
+    initialize_list (&c_list);
+
+    /* create one more list of INTs and initialize it */
+    linked_list i_list;
+    initialize_list (&i_list);
+
+    /* create some complex numbers */
+    complex a;
+    a.real = 3;
+    a.im = -2;
+
+    complex b;
+    b.real = 9;
+    b.im = 3;
+
+    complex c;
+    c.real = 8;
+    c.im = -12;
+   
+    complex d;
+    d.real = 7;
+    d.im = -9;
+
+    /* create some integers */
+    int x = 9;
+    int y = 7;
+    int z = 5;
+    
+    /* add numbers to list */
+    add_head (&c_list, &a);
+    add_head (&c_list, &b);
+    add_tail (&c_list, &c);
+    add_tail (&c_list, &d);
+
+    add_head (&i_list, &x);
+    add_tail (&i_list, &y);
+    add_head (&i_list, &z);
+    
+    /* try delete, reverse and show functions */
+    printf ("\n---list of complex numbers---\n");
+    delete (&c_list, (COMPARE)compare_complex, &b);
+    reverse_list (&c_list);
+    show_list (&c_list, (SHOW)show_complex);
+
+    printf ("\n---list of integers---\n");
+    reverse_list (&i_list);
+    delete (&i_list, (COMPARE)compare_integers, &y);
+    show_list (&i_list, (SHOW)show_integer);
+
+    return 0;
+}
+    
+void show_complex (complex* num) {
+    if (num -> im == 0) {
+	printf ("%d;\n", num -> real);
+    }
+    else {
+        printf ("\n%d%ci%d;\n", num -> real,
+	        num -> im > 0 ? '+' : '-',
+	        num -> im < 0 ? num -> im * (-1) : num -> im);
+    }
+}
+
+int compare_complex (complex* a, complex* b) {
+    if (a -> real == b -> real && a -> im == b -> im)
+	return 0;
+    else
+	return 1;
+}
+
+
+void show_integer (int* num) {
+    printf ("\n%d\n",  *num);
+}
+
+int compare_integers (int* a, int* b) {
+    if (*a == *b)
+	return 0;
+    else
+	return 1;
+}
--- linkedlist.h
+++ linkedlist.h
@@ -1,6 +1,9 @@
-/* C linked list implementation *
- *                                     *
- * Skupoy Sergey. 2014                 */
+/* C linked list implementation.              *
+ * any data type acceptable.                  *
+ * Functions for comparing and showing        *
+ * type variables must be defined separately. *
+ *                                            *
+ * Skupoy Sergey. 2014                        */
 
 #ifndef LINKEDLIST_H
 #define LINKEDLIST_H
@@ -8,81 +11,140 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct node {
-    int data;
-    struct node *next_node;
-} list_node, *list_node_ptr;
+/* function pointer to compare and show functions *
+ *                                                */
+typedef int (*COMPARE) (void*, void*);
+typedef void (*SHOW) (void*);
 
+/* node structure */
+typedef struct _node {
+    void *data;
+    struct _node *next;
+} list_node;
 
-void insert (list_node_ptr *list_head, int data)
-{
+/* list structure */
+typedef struct _linked_list {
+    list_node *head;
+    list_node *tail;
+} linked_list;
+
+/* list initialization */
+void initialize_list (linked_list *list) {
+    list -> head = NULL;
+    list -> tail = NULL;
+}
+
+/* add node to head */
+void add_head (linked_list *list, void *data) {
     /* create new node and allocate memory */
-    list_node_ptr new_node;
-    new_node = malloc (sizeof (list_node));
+    list_node *node = (list_node*) malloc (sizeof (list_node));
 
-    /* if memory is allocated successfully insert new node */
-    if (new_node != NULL) {
-        new_node -> data = data;
-        new_node -> next_node = *list_head;
-        *list_head = new_node;
+    /* check if memory is available */
+    if (node == NULL) {
+	printf ("\nNo memory available!\n");
+	return;
     }
-    else
-        printf("%d not inserted. No memory available.\n", data);
-}
 
-void delete (list_node_ptr *list_head, int data)
-{
-    list_node_ptr prev_ptr, curr_ptr, temp_ptr;
-
-    /* if data == head.data, then set new list head and delete node */
-    if (data == (*list_head) -> data) {
-        temp_ptr = *list_head;
-        *list_head = (*list_head) -> next_node;
-        free (temp_ptr);
-        printf ("%d deleted successfully\n", data);
+    /* add new node to head */
+    node -> data = data;
+    if (list -> head == NULL) {
+	list -> tail = node;
+	node -> next = NULL;
     }
     else {
-        prev_ptr = *list_head;
-        curr_ptr = (*list_head) -> next_node;
-
-	/* find node with data or reach the end of list */
-        while (curr_ptr != NULL && curr_ptr -> data != data) {
-            prev_ptr = curr_ptr;
-            curr_ptr = curr_ptr -> next_node;
-        }
-
-	/* if data is present in list, delete that node */
-        if (curr_ptr != NULL) {
-            temp_ptr = curr_ptr;
-            prev_ptr -> next_node = curr_ptr -> next_node;
-            free (temp_ptr);
-            printf ("%d deleted successfully\n", data);
-        }
-	else
-	    printf ("%d is not present in list.\n", data);
+	node -> next = list -> head;
     }
+    
+    list -> head = node;
 }
 
-/*check if list is empty */
-int is_empty_list (list_node_ptr list_head)
-{
-    return list_head == NULL;
-}
+/* add node to the end of list */
+void add_tail (linked_list *list, void *data) {
+    list_node *node = (list_node*) malloc (sizeof (list_node));
 
-/* print list elements */
-void print_list (list_node_ptr curr_ptr)
-{
-    if (curr_ptr == NULL)
-        printf("List is empty.\n\n");
+    /* check if memory is available */
+    if (node == NULL) {
+	printf ("\nNo memory available!\n");
+	return;
+    }
+
+    /* add new node to the end of list */
+    node -> data = data;
+    node -> next = NULL;
+    if (list -> head == NULL) {
+	list -> head = node;
+    }
     else {
-        printf("The list:\n");
+	list -> tail -> next = node;
+    }    
+    list -> tail = node;
+}
 
-        while (curr_ptr != NULL) {
-            printf("%d-> ", curr_ptr -> data);
-            curr_ptr = curr_ptr -> next_node;
-        }
-        printf ("NULL\n\n");
+/*delete node if it contains "data" value */
+void delete (linked_list *list, COMPARE compare, void *data) {
+    list_node *node = list -> head;
+    if (compare (node -> data, data) == 0) {
+	list -> head = node -> next;
+	free (node);
+	printf ("\nNode deleted successfully\n");
+	return;
     }
+    else {
+        list_node *prev = node;
+        node = node -> next;
+       
+        while (node != NULL) {
+	    /* if node is found delete it */
+	    if (compare (node -> data, data) == 0) {
+		prev -> next = node -> next;
+		free (node);
+		printf ("\nNode deleted successfully\n");
+		return;
+	    }
+	    prev = node;
+	    node = node -> next;
+	}
+	printf ("\nNode not found\n");
+    }
+}
+
+/* show list */
+void show_list (linked_list *list, SHOW show) {
+    if (list -> head == NULL) {
+	printf ("\nList is empty!\n");
+    }
+    else {
+        printf ("\nLinked list:\n");
+    
+        list_node *current = list -> head;
+
+        while (current != NULL) {
+	    show (current -> data);
+	    current = current -> next;
+        }
+    
+        printf ("\n__end of list__\n");
+    }
+}
+
+/* reverse list */
+void reverse_list (linked_list *list) {
+    list_node *current = list -> head;
+    list_node *prev = NULL;
+    list_node *next = current;
+
+    /* reassign "next" pointer for every node */
+    while (current != NULL) {
+	next = current -> next;
+	current -> next = prev;
+	prev = current;
+	current = next;
+    }
+
+    /* swap head and tail pointers */
+    list_node *tmp = list -> head;
+    list -> head = list -> tail;
+    list -> tail = tmp;
 }
 
 #endif

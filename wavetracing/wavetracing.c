7374adddb7774f6e7dafe474e671459aeac88248
Baldmaster <sergey.archlinux@gmail.com>
2015-01-08 10:05:31 +0200

    refactoring

---
+++ wavetracing/wavetracing.c
@@ -0,0 +1,122 @@
+#include "wavetracing.h"
+
+/* Coordinates increment */
+
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+
+
+/* looking for path in maze */
+int find_path (int* maze, int width, int height, wave_node *start, wave_node *target) {
+    /* initializing a list of utmost points in wavefront list*/
+    wave_node* wavefront = (wave_node*) malloc (sizeof (wave_node));
+    /* temp pointer*/
+    wave_node* wave;
+    /* insert starting point to wavefront */
+    insert (&wavefront, start -> x, start -> y);
+    /* mark number */
+    int n = 2;
+    /* temp variables */
+    int j, temp_x, temp_y;
+    /* setting starting point to 1 */
+    *(maze + ((start -> y) * height) + (start -> x)) = 1;
+
+    /* while there are points in wavefront */
+    while (wavefront) {
+
+	wave = wavefront;
+	/* traversing all points in list */
+	while (wave) {
+	    /* check all four sides */
+	    for (j = 0; j < 4; j++) {
+		/* adding increment values to coordinates */
+		temp_x = (wave -> x) + dx[j];
+		temp_y = (wave -> y) + dy[j];
+		
+		/* return success if reached target point */
+		if (temp_x == target -> x && temp_y == target -> y) {
+		    *(maze + (temp_y * height) + temp_x) = n;
+		    /* free wavefront list */
+		    empty_wavefront (&wavefront);
+		    free (wavefront);
+		    /* return success value */
+		    printf ("\n----- Path found! Length: %d -----\n", n);
+		    return 1;
+		}
+		
+		/* if out of bounds goto next side checking */
+		if (temp_x < 0 || temp_x > width || temp_y < 0 || temp_y > height)
+		    continue;
+		
+	        /* if point is free, mark it and insert new point to wavefront */
+		if (*(maze + (temp_y * height) + temp_x) == 0) {
+		    *(maze + (temp_y * height) + temp_x) = n;
+		    /* if insertion failed return failure value */
+		    if (!(insert (&wavefront, temp_x, temp_y)))
+			return -1;
+		}
+	    }
+	    /* delete current point from list */
+	    wave_node* temp = wave;
+	    delete (&wavefront, &temp);
+	    /* go to next point */
+	    wave = wave -> next;
+	}
+	/* incrementing mark number */
+	n++;
+    }
+    /* if there are no more free points and target point not reached, return failure */
+    printf ("\n----- PATH NOT FOUND -----\n");
+    /* free wavefront list */
+    empty_wavefront (&wavefront);
+    free (wavefront);
+    /* return fail value */
+    return 0;
+}
+
+
+int insert (wave_node** start, int x, int y) {
+    wave_node* node = (wave_node*) malloc (sizeof (wave_node));
+    if (node == NULL)
+	return 0;
+    else {
+	node -> x = x;
+	node -> y = y;
+	if ((*start) == NULL) {
+	    node -> next = NULL;
+	}
+	else {
+	    node -> next = *start;
+	    (*start) -> prev = node;
+	}
+	node -> prev = NULL;
+	*start = node;
+	return 1;
+    }
+}
+    		
+void delete (wave_node** start, wave_node** wave) {
+    if (*start == *wave) {
+	free (*wave);
+	*start = NULL;
+    }
+    else {
+	wave_node* prev = (*wave) -> prev;
+	wave_node* next = (*wave) -> next;
+	prev -> next = next;
+	if (next)
+	    next -> prev = prev;
+	free (*wave);
+    }
+
+}
+
+void empty_wavefront (wave_node** start) {
+    wave_node* temp = (*start);
+    wave_node* next = (*start);
+    while (temp != NULL) {
+	next = next -> next;
+	free (temp);
+	temp = next;
+    }
+}
--- wavetracing/wavetracing.h
+++ wavetracing/wavetracing.h
@@ -4,6 +4,9 @@
    linkedlist is used for saving wavefront points.
 
  ************************************************************/
+#ifndef WAVETRACING_H
+#define WAVETRACING_H
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -23,7 +26,7 @@ typedef struct _wave_node {
 /************************************************************
   Delete all list nodes. Gets first node of list as argument.
 ************************************************************/
-void empty_wavefront (wave_node** wavefront);
+void empty_wavefront (wave_node**);
 
 /************************************************************
    Find path function. First argument is a pointer to
@@ -32,107 +35,16 @@ void empty_wavefront (wave_node** wavefront);
  height values. Two last are start and target node (point), 
  check their values!
  ************************************************************/
-void find_path (int* maze, int width, int height, wave_node *start, wave_node *target);
+int find_path (int*, int, int, wave_node*, wave_node*);
 
 /************************************************************
    Insert new node into wavefront points list.
  ************************************************************/
-void insert (wave_node**, int, int);
+int insert (wave_node**, int, int);
 
 /************************************************************
    Delete node from wavefront points list.
  ************************************************************/
 void delete (wave_node**, wave_node**);
 
-/************************************************************
-   Coordinates increment
- ************************************************************/
-const int dx[4] = {1, 0, -1, 0};
-const int dy[4] = {0, 1, 0, -1};
-
-
-void find_path (int* maze, int width, int height, wave_node *start, wave_node *target) {
-    wave_node* wavefront = (wave_node*) malloc (sizeof (wave_node));
-    wave_node* wave;
-    insert (&wavefront, start -> x, start -> y);
-    int n = 2;
-    int j, temp_x, temp_y;
-    *(maze + ((start -> y) * height) + (start -> x)) = 1;
-    while (wavefront) {
-	wave = wavefront;
-	while (wave) {
-	    for (j = 0; j < 4; j++) {
-		temp_x = (wave -> x) + dx[j];
-		temp_y = (wave -> y) + dy[j];
-		if (temp_x == target -> x && temp_y == target -> y) {
-		    *(maze + (temp_y * height) + temp_x) = n;
-		    empty_wavefront (&wavefront);
-		    free (wavefront);
-		    printf ("\n----- Path found! Length: %d -----\n", n);
-		    return;
-		}
-		if (temp_x < 0 || temp_x > width || temp_y < 0 || temp_y > height)
-		    continue;
-	      
-		if (*(maze + (temp_y * height) + temp_x) == 0) {
-		    *(maze + (temp_y * height) + temp_x) = n;
-		    insert (&wavefront, temp_x, temp_y);
-		}
-	    }
-	    wave_node* temp = wave;
-	    delete (&wavefront, &temp);	   
-	    wave = wave -> next;
-	}
-	n++;
-    }
-    printf ("\n----- PATH NOT FOUND -----\n");
-    empty_wavefront (&wavefront);
-    free (wavefront);
-}
-    
-void insert (wave_node** start, int x, int y) {
-    wave_node* node = (wave_node*) malloc (sizeof (wave_node));
-    if (node == NULL)
-	printf ("\nNo memory!!!\n");
-    else {
-	node -> x = x;
-	node -> y = y;
-	if ((*start) == NULL) {
-	    node -> next = NULL;
-	}
-	else {
-	    node -> next = *start;
-	    (*start) -> prev = node;
-	}
-	node -> prev = NULL;
-	*start = node;
-    }
-}
-    
-		
-void delete (wave_node** start, wave_node** wave) {
-    if (*start == *wave) {
-	free (*wave);
-	*start = NULL;
-    }
-    else {
-	wave_node* prev = (*wave) -> prev;
-	wave_node* next = (*wave) -> next;
-	prev -> next = next;
-	if (next)
-	    next -> prev = prev;
-	free (*wave);
-    }
-
-}
-
-void empty_wavefront (wave_node** start) {
-    wave_node* temp = (*start);
-    wave_node* next = (*start);
-    while (temp != NULL) {
-	next = next -> next;
-	free (temp);
-	temp = next;
-    }
-}
-    
+#endif

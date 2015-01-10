469ef831f4cede6a8136a1c08dd1662f4891f557
Baldmaster <sergey.archlinux@gmail.com>
2015-01-10 11:41:11 +0200

    tabs to spaces

--- wavetracing/main.c
+++ wavetracing/main.c
@@ -13,32 +13,32 @@ int init (FILE*, int* width, int* height, int* x1, int* y1, int* x2, int* y2);
 int main (int argc, char* argv[]) {
     FILE* in;
     if (argc != 2) {
-	printf ("Usage: %s filename\n", argv[0]);
-	exit (EXIT_FAILURE);
+        printf ("Usage: %s filename\n", argv[0]);
+        exit (EXIT_FAILURE);
     }
   
     if ((in = fopen (argv[1], "r")) == NULL) {
-	printf ("Failed to open file \"%s\"! Does it exist?", argv[1]);
-	exit (EXIT_FAILURE);
+        printf ("Failed to open file \"%s\"! Does it exist?", argv[1]);
+        exit (EXIT_FAILURE);
     }
     char str[LIMIT + 1];
     int width, height, j, i = 0;
     int start_x, start_y, target_x, target_y;
 
     if (init (in, &width, &height, &start_x, &start_y, &target_x, &target_y)) {
-	printf ("Check your input file!\n");
-	exit (EXIT_FAILURE);
+        printf ("Check your input file!\n");
+        exit (EXIT_FAILURE);
     }
 
     unsigned char maze[height][width];
     i = 0;
     while ((fscanf (in, "%s", str)) == 1) {
-	j = 0;
-	while (str[j] != '\0') {
-	    maze[i][j] = str[j];
-	    j++;
-	}
-	i++;
+        j = 0;
+        while (str[j] != '\0') {
+            maze[i][j] = str[j];
+            j++;
+        }
+        i++;
     }
 
     list_node start, target;
@@ -63,28 +63,28 @@ int init (FILE* in, int* width, int* height, int* x1, int* y1, int* x2, int* y2)
     char str[LIMIT + 1];
     fgets (str, 33, in);
     if (sscanf (str, "%d", width) != 1) {
-	return 1;
+        return 1;
     }
-    
+
     fgets (str, 33, in);
     if (sscanf (str, "%d", height) != 1) {
-	return 1;
+        return 1;
     }
 
     fgets (str, 32, in);
     if (sscanf (str, "%d %d", x1, y1) != 2) {
-	return 1;
+        return 1;
     }
     
     fgets (str, 32, in);
     if (sscanf (str, "%d %d", x2, y2) != 2) {
-	return 1;
+        return 1;
     }
 
     if (*width < 10 || *width > LIMIT || *height < 10 || *height > 32 ||
-	*x1 < 0 || *x1 > *width || *y1 < 0 || *y1 > *height ||
-	*x2 < 0 || *x2 > *width || *y2 < 0 || *y2 > *height)
-	return 1;
+        *x1 < 0 || *x1 > *width || *y1 < 0 || *y1 > *height ||
+        *x2 < 0 || *x2 > *width || *y2 < 0 || *y2 > *height)
+        return 1;
 
     return 0;
 }
--- wavetracing/wavetracing.c
+++ wavetracing/wavetracing.c
@@ -27,58 +27,58 @@ int find_path (unsigned char* maze, int width, int height, list_node* start, lis
     /* while wavefront is not empty */
     while (wavefront) {
         /* assing head of wavefront list to current_node pointer */
-	current_node = wavefront;
-	
-	/* traversing all points in list */
-	while (current_node) {
-	    
-	    /* check all four sides */
-	    for (j = 0; j < 4; j++) {
-		
-		/* adding increment values to coordinates */
-		temp_x = (current_node -> x) + dx[j];
-		temp_y = (current_node -> y) + dy[j];
-		
-		/* return success if reached target point */
-		if (temp_x == target -> x && temp_y == target -> y) {
-		    *(maze + (temp_y * height) + temp_x) = mark;
-		    
-		    /* free wavefront list */
-		    empty_wavefront (&wavefront);
-		    
-		    /* return success value */
-		    printf ("\n----- Path found! Length: %d -----\n", mark);
-		    return 0;
-		}
-		/* if out of bounds goto next side checking */
-		if (temp_x < 0 || temp_x > width || temp_y < 0 || temp_y > height)
-		    continue;
-		
-	        /* if nearby point is free, mark it and insert new point to head of wavefront */
-		if (*(maze + (temp_y * height) + temp_x) == '-') {
-		    *(maze + (temp_y * height) + temp_x) = mark;
-		    
-		    /* if insertion failed, free wavefront list and return failure value */
-		    if (insert (&wavefront, temp_x, temp_y)) {
-			empty_wavefront (&wavefront);
-			return 2;
-		    }
-		}
-	    }
+        current_node = wavefront;
+
+        /* traversing all points in list */
+        while (current_node) {
+
+            /* check all four sides */
+            for (j = 0; j < 4; j++) {
+
+                /* adding increment values to coordinates */
+                temp_x = (current_node -> x) + dx[j];
+                temp_y = (current_node -> y) + dy[j];
+
+                /* return success if reached target point */
+                if (temp_x == target -> x && temp_y == target -> y) {
+                    *(maze + (temp_y * height) + temp_x) = mark;
+
+                    /* free wavefront list */
+                    empty_wavefront (&wavefront);
+
+                    /* return success value */
+                    printf ("\n----- Path found! Length: %d -----\n", mark);
+                    return 0;
+                }
+                /* if out of bounds goto next side checking */
+                if (temp_x < 0 || temp_x > width || temp_y < 0 || temp_y > height)
+                    continue;
+
+                /* if nearby point is free, mark it and insert new point to head of wavefront */
+                if (*(maze + (temp_y * height) + temp_x) == '-') {
+                    *(maze + (temp_y * height) + temp_x) = mark;
+
+                    /* if insertion failed, free wavefront list and return failure value */
+                    if (insert (&wavefront, temp_x, temp_y)) {
+                        empty_wavefront (&wavefront);
+                        return 2;
+                    }
+                }
+            }
 	    /* delete current point from list and goto next point*/
 	    list_node* temp = current_node;
-	    current_node = current_node -> next;
-	    delete (&wavefront, &temp);
-	}
-	/* incrementing mark value */
+            current_node = current_node -> next;
+            delete (&wavefront, &temp);
+        }
+        /* incrementing mark value */
 	mark++;
     }
     /* if there are no more free points and target point not reached, return failure */
     printf ("\n----- PATH NOT FOUND -----\n");
-    
+
     /* free wavefront pointer */
     free (wavefront);
-    
+
     /* return fail value */
     return 1;
 }
@@ -91,45 +91,45 @@ void show_path (unsigned char* maze, int width, int height, int x, int y) {
     *(maze + (y * height) + x) = 'F';
     int i, j, temp_x, temp_y;
     while (count) {
-	for (j = 0; j < 4; j++) {
-	    /* adding increment values to coordinates */
-	    temp_x = x + dx[j];
-	    temp_y = y + dy[j];
-	    
-	    /* if out of bounds goto next side checking */
-	    if (temp_x < 0 || temp_x > width || temp_y < 0 || temp_y > height)
-		    continue;
-	    
-	    /* mark path point with letter showing next step direction
+        for (j = 0; j < 4; j++) {
+            /* adding increment values to coordinates */
+            temp_x = x + dx[j];
+            temp_y = y + dy[j];
+            
+            /* if out of bounds goto next side checking */
+            if (temp_x < 0 || temp_x > width || temp_y < 0 || temp_y > height)
+                continue;
+
+            /* mark path point with letter showing next step direction
              * u - up, d - down, l - left, r - right                  */
-	    if (*(maze + (temp_y * height) + temp_x) == count) {
-	        switch (j) {
-	    	    case 0:
-	    	        *(maze + (temp_y * height) + temp_x) = 'l';
-	    	        break;
-	    	    case 1:
-	    	        *(maze + (temp_y * height) + temp_x) = 'u';
-	    	        break;
-	    	    case 2:
-	    	        *(maze + (temp_y * height) + temp_x) = 'r';
-	    	        break;
-	    	    case 3:
-	    	        *(maze + (temp_y * height) + temp_x) = 'd';
-	        }
-		x = temp_x;
-		y = temp_y;
-	        break;	    
-	    }		
-	}
+            if (*(maze + (temp_y * height) + temp_x) == count) {
+                switch (j) {
+                    case 0:
+                        *(maze + (temp_y * height) + temp_x) = 'l';
+                        break;
+                    case 1:
+                        *(maze + (temp_y * height) + temp_x) = 'u';
+                        break;
+                    case 2:
+                        *(maze + (temp_y * height) + temp_x) = 'r';
+                        break;
+                    case 3:
+                        *(maze + (temp_y * height) + temp_x) = 'd';
+                }
+                x = temp_x;
+                y = temp_y;
+                break;	    
+            }		
+        }
         count--;
     }
-    
+
     /* display maze with path */
     for (i = 0; i < height; i++) {
-	for (j = 0; j < width; j++)
-	    printf ("%2c", *(maze + (i* height) + j) < 33 ?
-		    '-' : *(maze + (i* height) + j));
-	printf ("\n");
+        for (j = 0; j < width; j++)
+            printf ("%2c", *(maze + (i* height) + j) < 33 ?
+                    '-' : *(maze + (i* height) + j));
+        printf ("\n");
     }
 }
 	
@@ -139,20 +139,20 @@ void show_path (unsigned char* maze, int width, int height, int x, int y) {
 int insert (list_node** start, int x, int y) {
     list_node* node = (list_node*) malloc (sizeof (list_node));
     if (node == NULL)
-	return 1;
+        return 1;
     else {
-	node -> x = x;
-	node -> y = y;
-	if ((*start) == NULL) {
-	    node -> next = NULL;
-	}
-	else {
-	    node -> next = *start;
-	    (*start) -> prev = node;
-	}
-	node -> prev = NULL;
-	*start = node;
-	return 0;
+        node -> x = x;
+        node -> y = y;
+        if ((*start) == NULL) {
+            node -> next = NULL;
+        }
+        else {
+            node -> next = *start;
+            (*start) -> prev = node;
+        }
+        node -> prev = NULL;
+        *start = node;
+        return 0;
     }
 }
 
@@ -182,8 +182,8 @@ void empty_wavefront (list_node** start) {
     list_node* temp = (*start);
     list_node* next = (*start);
     while (temp != NULL) {
-	next = next -> next;
-	free (temp);
-	temp = next;
+        next = next -> next;
+        free (temp);
+        temp = next;
     }
 }

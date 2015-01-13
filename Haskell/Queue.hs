44886d8f1f7f938349ce1d5d6d2fcc6e3aa9deba
Baldmaster <sergey.archlinux@gmail.com>
2015-01-13 16:37:10 +0200

    Queue.hs created

---
+++ Haskell/Queue.hs
@@ -0,0 +1,23 @@
+module Queue(Queue, enqueue, dequeue, emptyQueue, isEmptyQueue) where
+
+newtype Queue a = Q ([a], [a])
+
+isEmptyQueue (Q ([], [])) = True
+isEmptyQueue _            = False
+
+emptyQueue                = Q ([], [])
+
+enqueue x (Q ([], []))    = Q ([x], [])
+enqueue y (Q (xs, ys))    = Q (xs, y:ys)
+
+dequeue (Q ([], []))      = error "Error dequeue. Empty queue!"
+dequeue (Q ([], xs))      = Q (tail (reverse xs), [])
+dequeue (Q (x:xs, ys))    = Q (xs, ys)
+
+front (Q ([], []))        = error "front: empty queue!"
+front (Q ([], ys))        = last ys
+fornt (Q (x:xs, ys))      = x
+
+instance (Show a) => Show (Queue a) where
+  showsPrec p (Q (front, rear)) str
+    = showString "Q " (showList (front ++ reverse rear) str)

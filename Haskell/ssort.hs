a2ed29200b6b09b31d9ef66839a02e917b6ba94a
Baldmaster <sergey.archlinux@gmail.com>
2015-01-13 10:01:46 +0200

    selection sort

--- Haskell/ssort.hs
+++ Haskell/ssort.hs
@@ -2,11 +2,12 @@
 --
 -- Skupoy Sergey.  sergey.archlinux@gmail.com, 2015
 --
-
+split :: Ord a => [a] -> a -> [a] -> [a]
 split [] m r     = m : (ssort r)
 split (x:xs) m r = case x < m of
                        True  -> split xs x (m:r)
                        False -> split xs m (x:r)
 
+ssort :: Ord a => [a] -> [a]
 ssort []     = []
 ssort (x:xs) = split xs x []

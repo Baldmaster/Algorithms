d57a22f96861aa44ec1bf22fa0d1ba894c469cd3
Baldmaster <sergey.archlinux@gmail.com>
2015-01-14 14:54:01 +0200

    stack.hpp created

---
+++ Cpp/stack.hpp
@@ -0,0 +1,46 @@
+#ifndef STACK_HPP
+#define STACK_HPP
+#include <iostream>
+#include <memory>
+
+
+template <typename T>
+class Stack {
+    struct Node {
+        T data;
+        struct Node* next = nullptr;
+    };
+private:
+    struct Node* top = nullptr;
+public:
+    void push (T);
+    bool pop ();
+};
+
+template <typename T>
+void Stack<T>::push(T data) {
+    struct Node* newNode = new struct Node;
+    newNode -> data = data;
+    std::cout << data << std::endl;
+    if (this -> top == nullptr)
+        newNode -> next = nullptr;
+    else 
+        newNode -> next = this -> top;
+
+    this -> top = newNode;
+}
+
+template <typename T>
+bool Stack<T>::pop() {
+    if (this -> top == nullptr)
+        return false;
+    else {
+        struct Node* temp = this -> top;
+        this -> top = temp -> next;
+        delete temp;
+    }
+    
+    return true;
+}
+
+#endif

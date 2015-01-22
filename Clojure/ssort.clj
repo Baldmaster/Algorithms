295964f6dd9afcd0f25ee1d80bec8e338ad472f2
Baldmaster <sergey.archlinux@gmail.com>
2015-01-22 22:03:24 +0200

    selection sort created

---
+++ Clojure/ssort.clj
@@ -0,0 +1,22 @@
+;; Selection sort
+;;
+;; Skupoy Sergey. sergey.archlinux@gmail.com, 2015.
+
+(defn _split [arr key acc]
+  (loop [a arr k key ac acc]
+    (if (empty? a)
+      (cons k (ssort ac))
+      (if (< (first a) k)
+        (recur (rest a) (first a) (cons k ac))
+        (recur (rest a) k (cons (first a) ac))))))
+
+(defn ssort [arr]
+  (if (empty? arr)
+    '()
+    (_split (rest arr) (first arr) '())))
+
+
+   
+
+
+

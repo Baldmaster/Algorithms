ef1083fafb0ece1c51f1597fa872bc84d9436480
Baldmaster <sergey.archlinux@gmail.com>
2015-02-15 20:59:49 +0200

    topsort update

--- Haskell/TopologicalSort.hs
+++ Haskell/TopologicalSort.hs
@@ -1,9 +1,12 @@
 import Data.Array
 
+nodes :: Ix i => Array i e -> [i]
 nodes g = indices g 
 
+adjacent :: Ix i => Array i [(a, b)] -> i -> [a]
 adjacent g v = map fst (g ! v)
 
+inDegree :: (Ix i, Eq a) => Array i [(a, b)] -> a -> Int
 inDegree g n = length [t | v <- nodes g, t <- adjacent g v, (n == t)]
 
 topologicalSort :: Ix a => Array a [(a, b)] -> [a]

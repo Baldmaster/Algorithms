caaafeb3805fe49802615e366c41d5add4daa506
Baldmaster <sergey.archlinux@gmail.com>
2015-04-04 22:43:19 +0300

    Dijkstra.h created

---
+++ C/Dijkstra/Dijkstra.h
@@ -0,0 +1,36 @@
+#ifndef DIJKSTRA_H
+#define DIJKSTRA_H
+
+typedef struct _vertex {
+    int q_number;
+    int name;
+    int dist;
+    struct _vertex* p;
+    struct _node* adj;
+} vertex;
+
+typedef struct _node {
+    int weight;
+    vertex* vert;
+    struct _node* next;
+} node;
+
+typedef struct _bin_heap {
+    vertex** data;
+    int size;
+} bin_heap;
+
+
+typedef void (*SWAP) (void*, void*);
+typedef int (*COMPARE) (const void*, const void*);
+
+void heapify (bin_heap* heap, int index, COMPARE, SWAP);
+void build_heap (bin_heap* heap, COMPARE, SWAP);
+void sort_heap (bin_heap* heap, COMPARE, SWAP);
+int push (node** stack, vertex* v, int w);
+int parent (int);
+vertex* extract (bin_heap* h, COMPARE compare, SWAP swap);
+void change_key (bin_heap* h, vertex* v, SWAP swap);
+void dijkstra (bin_heap* heap, COMPARE compare, SWAP swap);
+
+#endif

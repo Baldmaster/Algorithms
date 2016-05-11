edac35fe3dfaa5373b90f8c7680f19c8b44925c9
Baldmaster <sergey.archlinux@gmail.com>
2016-05-11 15:47:14 +0300

    recursive DFS, BFS, test update

--- JS/test/test.js
+++ JS/test/test.js
@@ -19,22 +19,46 @@ graph.addEdge(c, g)
 graph.addEdge(f, e)
 
 describe("Testing DFS", function() {
-  it('Reach "E" from "A" should be TRUE', function() {
+  it('Search "E" from "A" should be TRUE', function() {
     expect(graph.dfs(a, e)).to.equal(true)
   })
-  it('Reach "G" from "A" should be TRUE', function() {
+  it('Search "G" from "A" should be TRUE', function() {
     expect(graph.dfs(a, g)).to.equal(true)
   })
-  it('Reach "E" from "B" should be TRUE', function() {
+  it('Search "E" from "B" should be TRUE', function() {
     expect(graph.dfs(b, e)).to.equal(true)
   })
-  it('Reach "C" from "B" should be FALSE', function() {
+  it('Search "C" from "B" should be FALSE', function() {
     expect(graph.dfs(b, c)).to.equal(false)
   })
-  it('Reach "D" from "C" should be FALSE', function() {
+  it('Search "D" from "C" should be FALSE', function() {
     expect(graph.dfs(c, d)).to.equal(false)
   })
-  it('Reach "G" from "F" should be FALSE', function() {
+  it('Search "G" from "F" should be FALSE', function() {
     expect(graph.dfs(f, g)).to.equal(false)
   })
 })
+
+describe("Testing BFS", function() {
+  it('Search "E" from "A" should be TRUE', function() {
+    expect(graph.bfs(a, e)).to.equal(true)
+  })
+  it('Search "G" from "A" should be TRUE', function() {
+    expect(graph.bfs(a, g)).to.equal(true)
+  })
+  it('Search "E" from "B" should be TRUE', function() {
+    expect(graph.bfs(b, e)).to.equal(true)
+  })
+  it('Search "C" from "B" should be FALSE', function() {
+    expect(graph.bfs(b, c)).to.equal(false)
+  })
+  it('Search "D" from "C" should be FALSE', function() {
+    expect(graph.bfs(c, d)).to.equal(false)
+  })
+  it('Search "G" from "F" should be FALSE', function() {
+    expect(graph.bfs(f, g)).to.equal(false)
+  })
+})
+
+
+  
